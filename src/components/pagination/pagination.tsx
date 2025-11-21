651b97b9080a10a8100d0a37b76128ec29fe1d47
ky7408 <ky7408@naver.com>
2025-11-21 10:54:36 +0900

    fix: 리뷰받은 코드 수정(#32)

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -4,8 +4,7 @@ import Arrow_left from '@/public/arrow_left.png';
 import Arrow_right from '@/public/arrow_right.png';
 import '@/components/pagination.css';
 const PAGE_BLOCK = 5; // 보여지는 버튼 개수
-const arrowLeft = Arrow_left;
-const arrowRight = Arrow_right;
+
 interface PaginationProps {
   currentPage: number;
   totalCount: number;
@@ -38,7 +37,7 @@ export default function Pagination({
   viewCount,
 }: PaginationProps) {
   const totalPage = Math.ceil(totalCount / viewCount); //토탈페이지 완료
-  if (totalPage <= 1) return null;
+  if (totalPage <= 1) return null; // 다음 페이지 까지 넘어갈 데이터가 없으면 페이지네이션 안보임
 
   const pageBtn = Array.from({ length: PAGE_BLOCK }, (_, i) => i + 1);
   const currentBlock = Math.floor((currentPage - 1) / PAGE_BLOCK);
@@ -64,7 +63,7 @@ export default function Pagination({
   return (
     <div className={paginationStyle()}>
       <button className={paginationBtnStyle()} disabled={currentPage === 1} onClick={handlePrev}>
-        <Image src={arrowLeft} alt="이전 페이지" />
+        <Image src={Arrow_left} alt="이전 페이지" />
       </button>
       {pageBtn.map((_, num) => {
         const pageNum = startPage + num;
@@ -84,7 +83,7 @@ export default function Pagination({
         disabled={currentPage === totalPage}
         onClick={handleNext}
       >
-        <Image src={arrowRight} alt="이전 페이지" />
+        <Image src={Arrow_right} alt="다음 페이지" />
       </button>
     </div>
   );
