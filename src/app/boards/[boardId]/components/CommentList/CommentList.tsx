584fb410d6f05911574235b0175dd0053ab57f44
Bangdayeon <dybang00@gmail.com>
2025-11-24 22:09:26 +0900

    feat: CommentList 컴포넌트 추가 (#17)

---
+++ src/app/boards/[boardId]/components/CommentList/CommentList.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { CommentType } from '@/types/CommentType';
+import { useState } from 'react';
+import Comment from '../Comment/Comment';
+import Pagination from '@/components/pagination/pagination';
+
+const VIEW_COUNT = 10;
+
+interface CommentListProps {
+  comments: CommentType[];
+}
+
+const CommentList = ({ comments }: CommentListProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // 댓글 리스트 잘라오기
+  const startIndex = (currentPage - 1) * VIEW_COUNT;
+  const endIndex = startIndex + VIEW_COUNT;
+  const currentComments = comments.slice(startIndex, endIndex);
+
+  return (
+    <div className="flex flex-col items-center gap-10">
+      <ul className="flex flex-col gap-6">
+        {currentComments.map((comment) => (
+          <li key={comment.id}>
+            <Comment comment={comment} />
+          </li>
+        ))}
+      </ul>
+
+      <Pagination
+        currentPage={currentPage}
+        totalCount={comments.length}
+        setPage={setCurrentPage}
+        viewCount={VIEW_COUNT}
+      />
+    </div>
+  );
+};
+
+CommentList.displayName = 'CommentList';
+export default CommentList;
