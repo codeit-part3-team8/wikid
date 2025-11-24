3a31a1ec0a4b21c52f246fec56db804654b247fb
ky7408 <ky7408@naver.com>
2025-11-24 12:39:54 +0900

    feat: 위키리스트 카드 구현 (#54)

---
+++ src/components/ListCard/ListCard.tsx
@@ -0,0 +1,57 @@
+import Avatar from '../Avatar/Avatar';
+import { tv } from 'tailwind-variants';
+import styles from './ListCard.module.css';
+import LinkCopy from '../LinkCopy/LinkCopy';
+
+// const profile = {
+//   id: 1,
+//   name: '양정훈',
+//   email: 'jh@example.com',
+//   image: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
+//   city: '서울',
+//   nationality: '대한민국',
+//   job: '개발자',
+// };
+
+const dummyCode = 'didwjdgns';
+
+interface ListCardProps {
+  image: string;
+  name: string;
+  city: string;
+  nationality: string;
+  job: string;
+}
+
+const listCardStyle = tv({
+  base: 'flex items-center flex-1 gap-[32px] py-6 px-9  shadow-[0px_4px_20px_rgba(0,0,0,0.08)]',
+});
+const cardInfoStyle = tv({
+  base: 'flex flex-col  flex-1 justify-between gap-[10px] md:gap-[14px] ',
+});
+
+const nameStyle = tv({
+  base: 'text-grayscale-500 text-xl font-semibold sm:text-[24px] sm:font-semibold',
+});
+
+export default function ListCard({ image, name, city, nationality, job }: ListCardProps) {
+  return (
+    <div className={listCardStyle()}>
+      <Avatar imgUrl={image} name={name} variant="list" />
+      <div className={cardInfoStyle()}>
+        <span className={nameStyle()}>{name}</span>
+        <div className={styles.text_style}>
+          <div className="text-grayscale-400 flex flex-col text-xs sm:text-sm">
+            <div>
+              {city},&nbsp;{nationality}
+            </div>
+            <div>{job}</div>
+          </div>
+          <div>
+            <LinkCopy code={dummyCode} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
