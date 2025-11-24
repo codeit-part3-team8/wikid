20ced12120c56c19ef3e3260a72e80e5c8bf3110
Bangdayeon <dybang00@gmail.com>
2025-11-25 01:41:11 +0900

    refactor: Divider에 orientation 속성 추가 (#81)

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,14 +1,27 @@
 import clsx from 'clsx';
+import { tv } from 'tailwind-variants';
+
+const style = tv({
+  base: 'bg-grayscale-200 rounded-full',
+  variants: {
+    orientation: {
+      horizontal: 'w-full h-px',
+      vertical: 'h-full w-px',
+    },
+  },
+});
 
 interface DividerProps {
   className?: string;
+  orientation?: 'horizontal' | 'vertical';
 }
 
-const Divider = ({ className }: DividerProps) => {
+const Divider = ({ className, orientation = 'horizontal' }: DividerProps) => {
   return (
     <div
       role="separator"
-      className={clsx('bg-grayscale-200 h-px w-full rounded-full', className)}
+      aria-orientation={orientation}
+      className={clsx(style({ orientation }), className)}
     />
   );
 };
