1702cfff7233a28c9cbf703e9b78c6224a07e0ce
Bangdayeon <dybang00@gmail.com>
2025-11-27 18:03:43 +0900

    chore: api 상수 및 에러 유틸 함수 작성 (#107)

---
+++ src/constants/api.ts
@@ -0,0 +1,12 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const API = {
+  BASE: BASE_URL,
+  USERS: `${BASE_URL}/users/`,
+  PROFILE: `${BASE_URL}/profiles/`,
+  NOTIFICATION: `${BASE_URL}/notifications`,
+  IMAGE: `${BASE_URL}/images/upload`,
+  COMMENT: `${BASE_URL}/comments`,
+  AUTH: `${BASE_URL}/auth`,
+  ARTICLES: `${BASE_URL}/articles/`,
+};
