4d31cef230714e0875a7a6d0673654c8c48912c6
ky7408 <ky7408@naver.com>
2025-11-29 17:49:51 +0900

    feat: 위키리스트 api 작업 및 카피 모달 (#127)

--- src/components/ListCard/ListCard.tsx
+++ src/components/ListCard/ListCard.tsx
@@ -1,18 +1,9 @@
 import Avatar from '../Avatar/Avatar';
 import { tv } from 'tailwind-variants';
 import LinkCopy from '../LinkCopy/LinkCopy';
-
-// const profile = {
-//   id: 1,
-//   name: '양정훈',
-//   email: 'jh@example.com',
-//   image: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
-//   city: '서울',
-//   nationality: '대한민국',
-//   job: '개발자',
-// };
-
-const dummyCode = 'didwjdgns';
+import Link from 'next/link';
+import SnackBar from '../SnackBar/SnackBar';
+import { useState } from 'react';
 
 interface ListCardProps {
   image: string;
@@ -20,10 +11,11 @@ interface ListCardProps {
   city: string;
   nationality: string;
   job: string;
+  code: string;
 }
 
 const listCardStyle = tv({
-  base: 'flex items-center h-[150px] md:h-[142px] gap-[32px] py-6 px-9  shadow-[0px_4px_20px_rgba(0,0,0,0.08)]',
+  base: 'flex items-center h-[150px] md:h-[142px] gap-[32px] py-6 px-9  shadow-[0px_4px_20px_rgba(0,0,0,0.08)] overflow-hidden',
 });
 const cardInfoStyle = tv({
   base: 'flex flex-col  flex-1 justify-between gap-[10px] md:gap-[14px] ',
@@ -33,24 +25,35 @@ const nameStyle = tv({
   base: 'text-grayscale-500 text-xl font-semibold sm:text-[24px] sm:font-semibold',
 });
 
-export default function ListCard({ image, name, city, nationality, job }: ListCardProps) {
+export default function ListCard({ code, image, name, city, nationality, job }: ListCardProps) {
+  const [showSnackBar, setShowSnackBar] = useState(false);
   return (
-    <div className={listCardStyle()}>
-      <Avatar imgUrl={image} name={name} variant="list" />
-      <div className={cardInfoStyle()}>
-        <span className={nameStyle()}>{name}</span>
-        <div className="flex items-end justify-between max-[641px]:items-start max-[640px]:flex-col max-[640px]:gap-[14px]">
-          <div className="text-grayscale-400 flex flex-col text-xs sm:text-sm">
-            <div>
-              {city},&nbsp;{nationality}
+    <>
+      <Link href={`/wiki/${code}`}>
+        <div className={listCardStyle()}>
+          <Avatar className="shrink-0" imgUrl={image} name={name} variant="list" />
+          <div className={cardInfoStyle()}>
+            <span className={nameStyle()}>{name}</span>
+            <div className="flex items-end justify-between max-[641px]:items-start max-[640px]:flex-col max-[640px]:gap-[14px]">
+              <div className="text-grayscale-400 flex flex-col text-xs sm:text-sm">
+                <div>
+                  {city}&nbsp;{nationality}
+                </div>
+                <div>{job}</div>
+              </div>
+              <div>
+                <LinkCopy code={code} onCopySuccess={() => setShowSnackBar(true)} />
+              </div>
             </div>
-            <div>{job}</div>
-          </div>
-          <div>
-            <LinkCopy code={dummyCode} />
           </div>
         </div>
-      </div>
-    </div>
+      </Link>
+      <SnackBar
+        isOpen={showSnackBar}
+        message="내 위키 링크가 복사되었습니다."
+        type="success"
+        onClose={() => setShowSnackBar(false)}
+      />
+    </>
   );
 }
